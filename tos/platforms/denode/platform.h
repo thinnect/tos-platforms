ae1f7a19740c3e114b94dec236c35794d5cffa10
Andres Vahter <andres.vahter@defendec.com>
2011-05-31 10:16:50 +0000

    Added WDT definitions to denode bootstrap.

--- tos/platforms/denode/platform.h
+++ tos/platforms/denode/platform.h
@@ -3,5 +3,15 @@
 
 /* Pin Change Interrupt Vectors */
 #define SIG_PIN_CHANGE0  _VECTOR(9)
- 
-#define platform_bootstrap() { MCUSR = 0; wdt_disable(); }
+
+/* Disable watchdog or enable with 8 second timeout if WDTON fuse is programmed. */
+/* (Disabling would result in a 16ms watchdog if WDTON is programmed and user
+ * applications might not initialize fast enough) */
+#define platform_bootstrap() { \
+    MCUSR &= ~(1<<WDRF); \
+	wdt_reset(); \
+    wdt_disable(); \
+    wdt_reset(); \
+    WDTCSR |= (1<<WDCE) | (1<<WDE); \
+    WDTCSR = (1<<WDP3) | (1<<WDP0); \
+}
